fe391499d74be1c7b6aea8cb8e2c673db3e50029
sagar-81 <163633212+sagar-81@users.noreply.github.com>
2024-09-15 10:39:07 +0530

    changes in router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginForm from './component/login/login'
-import SignUpForm from './component/signup/signup'
-import LandingPage from './component/landingpage/landing'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import LoginForm from './component/login/login';
+import SignUpForm from './component/signup/signup';
+import LandingPage from './component/landingpage/landing';
 
 function App() {
   return (
-
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/landing" />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
       </Routes>
-    </Router>
-
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
