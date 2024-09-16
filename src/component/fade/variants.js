eca92a8c02c302118e7808bee00a82af9e1c0187
Tushar004 <tushardushing004@gmail.com>
2024-09-16 18:29:59 +0530

    animation

---
+++ src/component/fade/variants.js
@@ -0,0 +1,16 @@
+// src/variants.js
+export const fadeInUp = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeOut',
+      },
+    },
+  };
+  
